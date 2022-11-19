8b3455c0e54fd840277f4573b2e5afc5d5c1edbb
Felipe Crema Ribeiro <100156699+felipetega@users.noreply.github.com>
2022-11-19 14:47:22 -0300

    ...

--- ProjetoIntegrador/JS/cliente.js
+++ ProjetoIntegrador/JS/cliente.js
@@ -1,14 +1,13 @@
 function cpf() {
-    var cpf = document.getElementById('cpf')
-    var cnpj = document.getElementById('cnpj')
-    cpf.style.display = 'block'
-    cnpj.style.display = 'none'
-  }
-  
-  function cnpj() {
-    var cpf = document.getElementById('cpf')
-    var cnpj = document.getElementById('cnpj')
-    cpf.style.display = 'none'
-    cnpj.style.display = 'block'
-  }
-  
+  var cpf = document.getElementById('cpf')
+  var cnpj = document.getElementById('cnpj')
+  cpf.style.display = 'block'
+  cnpj.style.display = 'none'
+}
+
+function cnpj() {
+  var cpf = document.getElementById('cpf')
+  var cnpj = document.getElementById('cnpj')
+  cpf.style.display = 'none'
+  cnpj.style.display = 'block'
+}
