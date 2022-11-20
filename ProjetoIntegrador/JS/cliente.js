ecd1fa6cc16f3b0fa745948fc25aa6169dabf5ef
Felipe Crema Ribeiro <100156699+felipetega@users.noreply.github.com>
2022-11-19 23:34:26 -0300

    ...

--- ProjetoIntegrador/JS/cliente.js
+++ ProjetoIntegrador/JS/cliente.js
@@ -3,6 +3,11 @@ function cpf() {
   var cnpj = document.getElementById('cnpj')
   cpf.style.display = 'block'
   cnpj.style.display = 'none'
+
+  var inputCPF = document.getElementById("inputCPF")
+  var inputCNPJ = document.getElementById("inputCNPJ")
+  inputCPF.required=true
+  inputCNPJ.required=false
 }
 
 function cnpj() {
@@ -10,4 +15,25 @@ function cnpj() {
   var cnpj = document.getElementById('cnpj')
   cpf.style.display = 'none'
   cnpj.style.display = 'block'
+
+  var inputCPF = document.getElementById("inputCPF")
+  var inputCNPJ = document.getElementById("inputCNPJ")
+  inputCPF.required=false
+  inputCNPJ.required=true
+}
+
+function mostraEndereco() {
+  var endereco = document.getElementById('endereco')
+  endereco.style.display = 'block'
+  cnpj.style.display = 'block'
+}
+
+function escondeEndereco() {
+  var endereco = document.getElementById('endereco')
+  endereco.style.display = 'none'
+}
+
+function sucesso() {
+  var sucesso = document.getElementById('sucesso')
+  sucesso.style.display = 'block'
 }
