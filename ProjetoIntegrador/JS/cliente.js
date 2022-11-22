8d38d39d6e4c826768ddb102b5aced3de486b710
Felipe Crema Ribeiro <100156699+felipetega@users.noreply.github.com>
2022-11-21 22:42:44 -0300

    ...

--- ProjetoIntegrador/JS/cliente.js
+++ ProjetoIntegrador/JS/cliente.js
@@ -24,16 +24,61 @@ function cnpj() {
 
 function mostraEndereco() {
   var endereco = document.getElementById('endereco')
+  var rua = document.getElementById('rua')
+  var estado = document.getElementById('estado')
+  var cidade = document.getElementById('cidade')
+  var numero = document.getElementById('numero')
+
+
   endereco.style.display = 'block'
-  cnpj.style.display = 'block'
+  rua.required = true
+  estado.required = true
+  cidade.required = true
+  numero.required = true
 }
 
 function escondeEndereco() {
   var endereco = document.getElementById('endereco')
+  var rua = document.getElementById('rua')
+  var estado = document.getElementById('estado')
+  var cidade = document.getElementById('cidade')
+  var numero = document.getElementById('numero')
+
+
   endereco.style.display = 'none'
+  rua.required = false
+  estado.required = false
+  cidade.required = false
+  numero.required = false
 }
 
 function sucesso() {
   var sucesso = document.getElementById('sucesso')
   sucesso.style.display = 'block'
+}
+
+function mostraPix(){
+
+}
+
+function mostraCartao(){
+  var cartao = document.getElementById('campoCartao')
+  var numeroCartao = document.getElementById('numeroCartao')
+  var numeroSeguranca = document.getElementById('numeroSeguranca')
+
+
+  cartao.style.display = 'block'
+  numeroCartao.required = true
+  numeroSeguranca.required = true
+}
+
+function escondeCartao(){
+  var cartao = document.getElementById('campoCartao')
+  var numeroCartao = document.getElementById('numeroCartao')
+  var numeroSeguranca = document.getElementById('numeroSeguranca')
+
+  
+  cartao.style.display = 'none'
+  numeroCartao.required = false
+  numeroSeguranca.required = false
 }
