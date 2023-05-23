77ffb0e404ad938f1f17f86364ded2c8ff3ebfb2
Felipe Crema Ribeiro <100156699+felipetega@users.noreply.github.com>
2023-05-23 16:59:23 -0300

    Add files via upload

---
+++ 2 semestre/web/web-app/Models/Carro.cs
@@ -0,0 +1,20 @@
+﻿namespace web_app.Models
+{
+    public class Carro
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cor { get; set; }
+        public DateTime DataFabricacao { get; set; }
+        public decimal Valor { get; set; }
+
+        public Carro()
+        {
+            this.Id = 0;
+            this.Nome = string.Empty;
+            this.Cor = string.Empty;
+            this.DataFabricacao = DateTime.Now;
+            this.Valor = 0;
+        }
+    }
+}
