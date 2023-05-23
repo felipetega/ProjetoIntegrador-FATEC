41e0f7ae2c20ba66cdf81c996f2ad38c690e446a
Felipe Crema Ribeiro <100156699+felipetega@users.noreply.github.com>
2023-05-23 16:58:13 -0300

    Add files via upload

---
+++ 2 semestre/web/web-app/Repositories/Configuration.cs
@@ -0,0 +1,10 @@
+﻿namespace web_app.Repositories
+{
+    public class Configuration
+    {
+            public static string getConnectionString()
+            {
+                return "Server=LAB02-D04\\SQLEXPRESS;Database=Loja;Trusted_Connection=True;TrustServerCertificate=True;";
+            }
+        }
+    }
