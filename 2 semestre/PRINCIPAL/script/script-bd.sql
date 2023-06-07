3ae25e47059de6a296d8299aaa36ded707b5e61f
Adriano <adriano.sampaio.santos.1979@gmail.com>
2023-06-07 09:26:21 -0300

    CheckBox
    Atualizado o mostrar/ocultar senha

--- 2 semestre/PRINCIPAL/script/script-bd.sql
+++ 2 semestre/PRINCIPAL/script/script-bd.sql
@@ -7,7 +7,7 @@ create table carro(
 	constraint pk_carro primary key (id)
 );
 
-
+USE [Loja]
 create table [login](
 	id int not null identity(1,1),
 	usuario varchar(100) not null,
@@ -19,3 +19,32 @@ insert into carro (nome, cor, dataFabricacao, valor)
 	values('Gol','branca', '1985-06-13', 15000);
 
 insert into login (usuario, senha) values ('zeca','123');
+
+insert into login (usuario, senha) values ('adriano','456');
+
+select *
+from [dbo].[login]
+
+use [Loja]
+create table postagem
+(
+	id int identity not null,
+	usuario varchar(50) not null,
+	descricao varchar(200) not null,
+	urlImagem varchar(max) not null,
+	curtidas int not null,
+	constraint pk_postagem primary key (id)
+);
+
+create table produto
+(
+	id int identity not null,
+	nome varchar(100) not null,
+	descricao varchar(200) not null,
+	preco decimal (5,2) not null,
+	urlImagem varchar (max) not null,
+	constraint pk_produto primary key (id)
+);
+
+select *	
+from [dbo].[produto]
--- 2 semestre/PRINCIPAL/web/web-app/wwwroot/js/site.js
+++ 2 semestre/PRINCIPAL/web/web-app/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function mostrarOcultarSenha(){
+    var senha = document.getElementById("senha");
+    if (senha.type == "password") {
+        senha.type = "text";
+    } else {
+        senha.type = "password";
+    }
+}
